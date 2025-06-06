c43765867d535c75c42c960aa258de89ecec0139
Aditya <adityachauhan2501@gmail.com>
2025-06-06 11:48:01 +0530

    08: agents setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,27 +1,5 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
 
 export function HomeView() {
-  const { data: session } = authClient.useSession();
-  const router = useRouter();
-  if (!session) {
-    return <p>Loading...</p>;
-  }
-
-  return (
-    <div className="flex flex-col p-4 gap-y-6">
-      <p>Logged in as {session.user.name}</p>
-      <Button
-        onClick={() =>
-          authClient.signOut({
-            fetchOptions: { onSuccess: () => router.push("/sign-in") },
-          })
-        }
-      >
-        Sign out
-      </Button>
-    </div>
-  );
+  return <div>Home View</div>;
 }
