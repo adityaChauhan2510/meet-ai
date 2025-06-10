c33a82d86727f7fccc53a86434916a3cce5ddc64
Aditya <adityachauhan2501@gmail.com>
2025-06-10 08:34:04 +0530

    15 : meetings form

--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -25,7 +25,7 @@ export const AgentsListHeader = () => {
       <NewAgentDialog open={isDialogOpen} onOpenChange={setIsDialogOpen} />
       <div className="py-4 px-4 md:px-8 flex flex-col gap-4">
         <div className="flex items-center justify-between">
-          <h5 className="font-medium text-xl">My Actions</h5>
+          <h5 className="font-medium text-xl">My Agents</h5>
           <Button onClick={() => setIsDialogOpen(true)}>
             <PlusIcon />
             New Agent
